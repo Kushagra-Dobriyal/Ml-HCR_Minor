596e1c895cc38444f1edb09ae9119f48a4a4f449
Nakulvarshney07 <nakulv.ug23.cs@nitp.ac.in>
2026-02-18 16:19:35 +0530

    Initial commit for minor project

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,15 @@
+
+import HeaderComp from "@/components/common/header";
+import React from "react";
+
+
+export default function Home() {
+
+
+  return (
+    <>
+    <HeaderComp />  
+    <div className="h-100"></div>
+    </>
+  );
+}
